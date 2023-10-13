85f710d217567f89dbb7e0c52e3fcae864ebf138
Rui Soares <rui.rprs3@gmail.com>
2023-10-14 00:25:08 +0100

    inicio do llopen

--- lab1/src/application_layer.c
+++ lab1/src/application_layer.c
@@ -5,6 +5,13 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
+    LinkLayer connectionParams = buildConnectionParams(serialPort, role, baudRate,
+                                                        nTries, timeout);
+
+    if(llopen(connectionParams) < 0){
+        printf("erro no llopen()\n");
+        exit();
+    }
     // open file (?)
     // llopen
     // retorna (in)sucesso
@@ -20,4 +27,26 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 
 
     // llclose
+}
+
+LinkLayer buildConnectionParams(const char *serialPort, const char *role, int baudRate,
+                               int nTries, int timeout) {
+    LinkLayer connectionParams;
+
+    // Copy serialPort into the LinkLayer struct.
+    strncpy(connectionParams.serialPort, serialPort, sizeof(connectionParams.serialPort) - 1);
+    connectionParams.serialPort[sizeof(connectionParams.serialPort) - 1] = '\0'; // Ensure null-termination.
+
+    connectionParams.baudRate = baudRate;
+    connectionParams.nRetransmissions = nTries;
+    connectionParams.timeout = timeout;
+
+    if(role == "tx"){
+        connectionParams.role = LinkLayerRole.LlTx;
+    }
+    else{
+        connectionParams.role = LinkLayerRole.LlRx;
+    }
+
+    return connectionParams;
 }
