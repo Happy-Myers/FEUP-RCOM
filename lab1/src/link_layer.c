4dc6b1d42900ccca99cac022739e00fb81c9bcb9
Rui Soares <rui.rprs3@gmail.com>
2023-10-16 15:32:28 +0100

    Going back on read_noncanonical file

--- lab1/src/link_layer.c
+++ lab1/src/link_layer.c
@@ -25,6 +25,60 @@ enum STATE {
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+int set_fd(LinkLayer conParam){
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(conParam.serialPort, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(conParam.serialPort);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = conParam.baudRate | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+    return fd;
+}
+
 
 int openChannel(int fd){
     
@@ -78,7 +132,7 @@ int llopen(LinkLayer connectionParameters)
     int fd = -1;
 
     if(connectionParameters.role == LlRx){
-        if((fd = read_noncanonical(connectionParameters)) > 0)
+        if((fd = set_fd(connectionParameters)) > 0)
             if(openChannel(fd) < 0) return -1;
     }
     else{
